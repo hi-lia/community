5a2c517f6abe70588f8a6e6c38ed5adfb1f661f4
Liao Meiting <94345355+hi-lia@users.noreply.github.com>
2023-10-23 02:04:28 +0800

    send mail done

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+    @Test
+    public void testMail() {
+        mailClient.sendMail("liaomeiting1@163.com", "test", "hahahhahahaha");
+    }
+
+    @Test
+    public void testHTMLMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+        mailClient.sendMail("liaomeiting1@163.com", "Welcome", content);
+    }
+}
