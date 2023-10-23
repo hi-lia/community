43695d48228316d48d568c67ea9c2679aff6c02c
Liao Meiting <94345355+hi-lia@users.noreply.github.com>
2023-10-23 22:34:49 +0800

    修改用户头像 done
    1. 在controller里面存图片，因为MultipartFile是SpringMVC对象，不要跟业务层产生耦合
    2. service层更新headerUrl就行了
    3. headerUrl会调用getHeader()路由，从而访问到上传的头像
    4. 当页面访问到了headerUrl时，浏览器会发送一个GET请求到这个URL。比如index.html中的这个标签<img th:src="${loginUser.headerUrl}" class="rounded-circle" style="width:30px;"/>发起的请求匹配到了getHeader()方法的路径定义，从而触发了getHeader()方法的调用。

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -1,16 +1,120 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.jni.OS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping(path="/setting", method = RequestMethod.GET)
-    public String getSettingPage(){
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getSettingPage() {
 
         return "/site/setting";
+    }
+
+    /**
+     * 功能：上传文件
+     * 请求方法：必须是post
+     * 注入：给loginUser设置头像，所以要从hostHolder里获取当前user
+     * 参数：MultipartFile对象，SpringMVC专门提供；
+     */
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        // headerImage为空
+        if (headerImage == null) {
+            model.addAttribute("error", "您还没有上传图片！");
+            return "/site/setting";
+        }
+        // 文件没错
+        // 存的文件名不能用原始的，我们随机生成名字，后缀保持原来的
+        String fileName = headerImage.getOriginalFilename();
+        //截取后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        // 图片后缀空的，提示格式不正确
+        if (StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "文件格式不正确");
+            return "/site/setting";
+        }
+        // 生成随机名字
+        fileName = CommunityUtil.generateUUID() + suffix;
+        // 确认文件存放位置
+        File dest = new File(uploadPath + "/" + fileName);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败：" + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常！", e);
+        }
+
+        // 更新当前用户的头像的路径（web访问路径）
+        // http://localhost:8080/community/user/header/xxx.pgn
+        // 获取当前用户
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    @RequestMapping(path = "/header/{fileName}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        // 服务器存放路径
+        fileName = uploadPath + "/" + fileName;
+        // 文件后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        // 响应图片
+        response.setContentType("image/" + suffix);
+        try (
+                OutputStream os = response.getOutputStream(); // response会负责关闭
+                FileInputStream fis = new FileInputStream(fileName); // 我们自己new出来的 手动关闭
+        ) {
+            //输出 一批批输出
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b = fis.read(buffer)) != -1) {
+                os.write(buffer, 0, b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败：" + e.getMessage());
+            //throw new RuntimeException(e);
+        }
     }
 }
