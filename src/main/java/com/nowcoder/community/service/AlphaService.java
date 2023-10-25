caf4165f0f930b0c4694ac532f396907f18bf9e4
Liao Meiting <94345355+hi-lia@users.noreply.github.com>
2023-10-25 11:59:28 +0800

    Transaction management 示例
    事务管理：
    1. propogation事务传播机制：在业务方法A中调用业务方法B，以B的事务为准呢还是A的呢？
    REQUIRED: 支持当前事务(外部事务A)，如果不存在则创建新事物B；
    REQUIRED_NEW: 创建一个新事物，并且暂停当前事务(外部事务A）
    NESTED: 如果当前存在事务(外部事务)，则嵌套在该事务中执行（独立的提交和回滚），否则就会和REQUIRED一样

---
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -0,0 +1,89 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.util.Date;
+
+@Service
+public class AlphaService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public String save1() {
+        // 添加新用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 发布新帖子
+        DiscussPost discussPost = new DiscussPost();
+        discussPost.setUserId(user.getId());
+        discussPost.setTitle("Hello");
+        discussPost.setContent("新人报道");
+        discussPost.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(discussPost);
+
+        Integer.valueOf("abc");
+
+        return "OK";
+    }
+
+
+    // 编程式事务
+    public String save2(){
+        transactionTemplate.setIsolationLevel((TransactionDefinition.ISOLATION_READ_COMMITTED));
+        transactionTemplate.setPropagationBehavior((TransactionDefinition.PROPAGATION_REQUIRED));
+        return transactionTemplate.execute(new TransactionCallback<String>() {
+            @Override
+            public String doInTransaction(TransactionStatus transactionStatus) {
+                // 添加新用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+                user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 发布新帖子
+                DiscussPost discussPost = new DiscussPost();
+                discussPost.setUserId(user.getId());
+                discussPost.setTitle("你好");
+                discussPost.setContent("我是新人！");
+                discussPost.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(discussPost);
+
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
+    }
+}
